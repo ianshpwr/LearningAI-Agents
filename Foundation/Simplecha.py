71c840ec5a2bac58448125376d00fe3a226ed05e
ansh <ianshpwr@gmail.com>
2025-06-19 22:00:07 +0530

    removed the grok this iq test

--- Foundation/Simplecha.py
+++ Foundation/Simplecha.py
@@ -5,8 +5,8 @@ import os
 gemini_api_key = os.getenv('GEMINI_API_KEY')
 grok_api_key = os.getenv('GROK_API_KEY')
 
-if gemini_api_key and grok_api_ley:
-    print(f"gemini and grok API Key exists and begins {gemini_api_key[:8]} and {grok_api_ley[:8]}")
+if gemini_api_key:
+    print(f"gemini and grok API Key exists and begins {gemini_api_key[:8]}")
 else:
     print("gemini or grok API Key not set - please head to the troubleshooting guide in the setup folder")
     
@@ -26,4 +26,3 @@ messages = [{"role": "user", "content": question}]
 
 response = model.generate_content(contents=messages[0]["content"])
 answer = response.text
-print(answer)
